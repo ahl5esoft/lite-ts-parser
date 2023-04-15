153e45ad409b1d60218fb73b10172c0ca5797df0
linyimin <ahl5esoft@gmail.com>
2023-04-15 09:53:19 +0800

    docs: v1.4.3

---
+++ src/to-value-parser_test.ts
@@ -0,0 +1,57 @@
+import { deepStrictEqual } from 'assert';
+import { Enum, EnumFactoryBase, EnumItem } from 'lite-ts-enum';
+import { Mock, mockAny } from 'lite-ts-mock';
+
+import { ToValueParser as Self } from './to-value-parser';
+
+describe('src/to-value-parser.ts', () => {
+    describe('.parse(v: any)', () => {
+        it('negative', async () => {
+            const mockEnumFactory = new Mock<EnumFactoryBase>();
+            const self = new Self(mockEnumFactory.actual);
+
+            const mockEnumService = new Mock<Enum<EnumItem>>();
+            mockEnumFactory.expectReturn(
+                r => r.build('ValueTypeData'),
+                mockEnumService.actual
+            );
+
+            mockEnumService.expectReturn(
+                r => r.get(mockAny),
+                {
+                    value: 1,
+                }
+            );
+
+            const res = await self.parse(`金币*13140`);
+            deepStrictEqual(res, {
+                count: 13140,
+                valueType: 1,
+            });
+        });
+
+        it('positive', async () => {
+            const mockEnumFactory = new Mock<EnumFactoryBase>();
+            const self = new Self(mockEnumFactory.actual);
+
+            const mockEnumService = new Mock<Enum<EnumItem>>();
+            mockEnumFactory.expectReturn(
+                r => r.build('ValueTypeData'),
+                mockEnumService.actual
+            );
+
+            mockEnumService.expectReturn(
+                r => r.get(mockAny),
+                {
+                    value: 1,
+                }
+            );
+
+            const res = await self.parse(`金币*13140`);
+            deepStrictEqual(res, {
+                count: 13140,
+                valueType: 1,
+            });
+        });
+    });
+});
