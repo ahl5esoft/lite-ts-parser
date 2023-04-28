93ad470017b34451d31b50f825d6c2baec2f5031
hourenjie <815972338@qq.com>
2023-04-28 17:55:30 +0800

    feat:修改单元测试

--- src/big-integer-exp_test.ts
+++ src/big-integer-exp_test.ts
@@ -6,7 +6,7 @@ describe('src/big-integer-exp.ts', () => {
     describe('.build(type: string)', () => {
         it('custom', () => {
             const self = new Self('(count)=>{return count*1000}');
-            const res = self.eval(1000000000000000000000);
+            const res = self.eval(10000000000000000e5);
             strictEqual(res, '1000000000000000000000000');
         });
     });
